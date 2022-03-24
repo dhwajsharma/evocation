c22a22fd6a8fa73fd26f9d965e32313284791787
Dhwaj Sharma <dhwajsharma10@gmail.com>
2022-03-24 13:53:26 +0530

    Added styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,48 @@
 import "./App.css";
+import evocation from "./images/evocation.png";
+import Container from "@mui/material/Container";
+import AppBar from "@mui/material/AppBar";
+import Typography from "@mui/material/Typography";
+import { Grid, Grow } from "@mui/material";
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
+import useStyles from "./styles";
 
 function App() {
-  return <div className="app"></div>;
+  const classes = useStyles();
+
+  return (
+    <Container maxWidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">
+          Evocation
+        </Typography>
+        <img
+          className={classes.image}
+          src={evocation}
+          alt="evocation"
+          height="60"
+        />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
+  );
 }
 
 export default App;
